6d55d9844ca50f7daabbdc9d44dbde212629d303
Michal Ceresnik <michal.ceresnik@gmail.com>
2019-03-01 13:21:18 +0100

    Recharge the battery and shield to full when not under attack and lower than 90%

--- CodingArena.Miso/MySuperHero.cs
+++ CodingArena.Miso/MySuperHero.cs
@@ -14,6 +14,8 @@ namespace CodingArena.Miso
         private const int shieldDamagedPercentage = 10;
         private int myLastShieldPercent = 100;
         private int myLastHealthPercent = 100;
+        private int batteryNotFullPercentage = 90;
+        private int shieldNotFullPercentage = 90;
 
         public ITurnAction GetTurnAction(IOwnBot ownBot, IReadOnlyCollection<IEnemy> enemies, IBattlefieldView battlefield)
         {            
@@ -23,6 +25,16 @@ namespace CodingArena.Miso
                 UpdateCurrentShieldAndHealth(ownBot);
                 return RechargeTheBattery();
             }
+            //if (NotUnderAttack(ownBot) && BatteryNotFullEnough(ownBot, batteryNotFullPercentage))
+            //{
+            //    UpdateCurrentShieldAndHealth(ownBot);
+            //    return RechargeTheBattery();
+            //}
+            //if (NotUnderAttack(ownBot) && ShieldNotFullEnough(ownBot, shieldNotFullPercentage))
+            //{
+            //    UpdateCurrentShieldAndHealth(ownBot);
+            //    return RechargeTheShieldToMaximum(ownBot);
+            //}
             if (IsShieldTooDamaged(ownBot, shieldDamagedPercentage))
             {
                 turnAction = HaveEnoughEnergyForFullShield(ownBot) ? RechargeTheShieldToMaximum(ownBot) : RechargeTheBattery();
@@ -47,6 +59,21 @@ namespace CodingArena.Miso
             }
             UpdateCurrentShieldAndHealth(ownBot);
             return turnAction;
+        }
+
+        private bool NotUnderAttack(IOwnBot ownBot)
+        {
+            return IsUnderAttack(ownBot) == false;
+        }
+
+        private bool BatteryNotFullEnough(IOwnBot ownBot, int batteryThreshold)
+        {
+            return ownBot.Energy.Percent < batteryThreshold;
+        }
+
+        private bool ShieldNotFullEnough(IOwnBot ownBot, int shieldNotFullThreshold)
+        {
+            return ownBot.Shield.Percent < shieldNotFullThreshold;
         }
 
         private void UpdateCurrentShieldAndHealth(IOwnBot ownBot)
